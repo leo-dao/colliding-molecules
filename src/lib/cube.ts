c863e1231dc29eb29ead56d31a768ae3cae6a7ba
Leo Dao <leotomdao@gmail.com>
2022-12-28 18:12:19 +0100

    Function to add cube with normals

---
+++ src/lib/cube.ts
@@ -0,0 +1,74 @@
+import * as THREE from 'three';
+
+export const createFace = (color: string) => {
+
+    // Creating a colored plane of size 1x1 
+    const geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({
+        color: color,
+        side: THREE.DoubleSide
+    });
+    const face = new THREE.Mesh(geometry, material);
+
+
+    // Creating a normal line from (0, 0, 0) to (0, 0, 1) 
+    const start = new THREE.Vector3(0, 0, 0);
+    const end = new THREE.Vector3(0, 0, 1);
+    const lineGeometry = new THREE.BufferGeometry().setFromPoints([start, end]);
+
+    const lineMaterial = new THREE.LineBasicMaterial({ color: color });
+
+    const line = new THREE.Line(lineGeometry, lineMaterial);
+
+    // Creating a group to be able to merge the face and the line
+    const group = new THREE.Group();
+    group.add(face);
+    group.add(line);
+
+    return group;
+}
+
+export const createCube = () => {
+
+    // Creating the cube faces
+    const frontFace = createFace('red');
+    const backFace = createFace('green');
+    const leftFace = createFace('blue');
+    const rightFace = createFace('yellow');
+    const topFace = createFace('orange');
+    const bottomFace = createFace('purple');
+
+    // Positioning the cube faces
+    // Adding or subtracting 0.5 to all coordinates to center cube at (0, 0, 0)
+    frontFace.position.set(0, 0, 0.5);
+    backFace.position.set(0, 0, -0.5);
+
+    leftFace.position.set(-0.5, 0, 0);
+    rightFace.position.set(0.5, 0, 0);
+
+    topFace.position.set(0, 0.5, 0);
+    bottomFace.position.set(0, -0.5, 0);
+
+
+    // Rotating the cube faces
+    frontFace.rotation.set(0, 0, 0);
+    backFace.rotation.set(0, Math.PI, 0);
+
+    leftFace.rotation.set(0, -Math.PI / 2, 0);
+    rightFace.rotation.set(0, Math.PI / 2, 0);
+
+    topFace.rotation.set(-Math.PI / 2, 0, 0);
+    bottomFace.rotation.set(Math.PI / 2, 0, 0);
+
+
+    // Creating a group and adding the cube faces to it
+    const cube = new THREE.Group();
+    cube.add(frontFace);
+    cube.add(backFace);
+    cube.add(leftFace);
+    cube.add(rightFace);
+    cube.add(topFace);
+    cube.add(bottomFace);
+
+    return cube;
+}
