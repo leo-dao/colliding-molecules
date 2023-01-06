83ef184808b062d8f948dc7f44f2abb356bb304d
Leo Dao <leotomdao@gmail.com>
2023-01-06 18:35:22 -0500

    Simplified/abstracted + added check for multiple collision

--- src/lib/collision.ts
+++ src/lib/collision.ts
@@ -1,98 +1,134 @@
 import * as THREE from 'three';
+import { mergeMolecules } from './geometry';
 
 // Storing bounding box and color of the faces and lines
 interface BoundingBox {
     box: THREE.Box3,
+    line: THREE.Line,
     color: THREE.Color
 }
 
-const getBoundingBoxes = (molecule: THREE.Object3D) => {
+export const getBoundingBoxes = (molecule: THREE.Object3D) => {
 
-    // Getting all the faces and lines belonging to the molecule
-    const faces = molecule.children.map((child: THREE.Object3D) => child.children[0] as THREE.Mesh);
-    const lines = molecule.children.map((child: THREE.Object3D) => child.children[1] as THREE.Line);
+    // Getting all cubes in the molecule
+    const cubes = molecule.children.filter(child => child.type === 'Group') as THREE.Group[];
+
+    const faces: THREE.Mesh[] = [];
+    const lines: THREE.Line[] = [];
+
+    // Getting all faces and lines in the cubes
+    cubes.forEach(cube => {
+        cube.children.forEach(child => {
+            faces.push(child.children[0] as THREE.Mesh)
+            lines.push(child.children[1] as THREE.Line)
+        })
+    });
 
     // Getting the bounding boxes of all the faces and lines and their colors
-    const facesBoxes = faces.map((face: THREE.Mesh) => {
+    const faceBoxes = faces.map((face: THREE.Mesh) => {
 
         const BoundingBoxFaces: BoundingBox = {
             box: new THREE.Box3().setFromObject(face),
+            line: lines[faces.indexOf(face)],
             // @ts-ignore, color does exist
             color: face.material.color
+
         }
 
         return BoundingBoxFaces;
-    }
-    );
+    });
 
-    const linesBoxes = lines.map((line: THREE.Line) => {
+    const lineBoxes = lines.map((line: THREE.Line) => {
 
         const BoundingBoxLines: BoundingBox = {
             box: new THREE.Box3().setFromObject(line),
+            line: line,
             // @ts-ignore
             color: line.material.color
         }
 
         return BoundingBoxLines;
-    }
-    );
+
+    });
 
     // Returning as an object
-    return { facesBoxes, linesBoxes };
+    return { faceBoxes, lineBoxes };
 
+}
+
+const checkBoundingBoxes = (molecule1: THREE.Object3D, molecule2: THREE.Object3D) => {
+
+    // Getting the bounding boxes of all faces and lines of the two molecules
+    const b1 = getBoundingBoxes(molecule1);
+    const b2 = getBoundingBoxes(molecule2);
+
+    // Variable to check if the molecules have already merged to avoid multiple merges
+    let merged = false;
+
+    b1.faceBoxes.forEach((faceBox1: BoundingBox) => {
+
+        // Checking if face boxes of molecule 1 are intersecting with face boxes of molecule 2
+        b2.faceBoxes.forEach((faceBox2: BoundingBox) => {
+            if (faceBox1.box.intersectsBox(faceBox2.box) && faceBox1.color.equals(faceBox2.color)) {
+
+                if (!merged) {
+                    merged = true;
+                    return mergeMolecules(molecule1, molecule2, faceBox1.line, faceBox2.line);
+                }
+            }
+        })
+
+        //checking if face boxes of molecule 1 are intersecting with line boxes of molecule 2
+        b2.lineBoxes.forEach((lineBox2: BoundingBox) => {
+            if (faceBox1.box.intersectsBox(lineBox2.box) && faceBox1.color.equals(lineBox2.color)) {
+
+                if (!merged) {
+                    merged = true;
+                    return mergeMolecules(molecule1, molecule2, faceBox1.line, lineBox2.line);
+                }
+            }
+        })
+    })
+
+    // Same as above but for line boxes
+    b1.lineBoxes.forEach((lineBox1: BoundingBox) => {
+
+        b2.faceBoxes.forEach((faceBox2: BoundingBox) => {
+            if (lineBox1.box.intersectsBox(faceBox2.box) && lineBox1.color.equals(faceBox2.color)) {
+
+                if (!merged) {
+                    merged = true;
+                    return mergeMolecules(molecule1, molecule2, lineBox1.line, faceBox2.line);
+                }
+            }
+        });
+
+        b2.lineBoxes.forEach((lineBox2: BoundingBox) => {
+            if (lineBox1.box.intersectsBox(lineBox2.box) && lineBox1.color.equals(lineBox2.color)) {
+
+                if (!merged) {
+                    merged = true;
+                    return mergeMolecules(molecule1, molecule2, lineBox1.line, lineBox2.line);
+                }
+
+            }
+        })
+    })
 }
 
 export const checkCollision = (molecules: THREE.Object3D[]) => {
 
-    // Iterating through all the molecules
-    for (var i = 0; i < molecules.length; i++) {
+    // Iterating over all molecules and if they are not the same molecule, check bounding boxes for collision
+    molecules.forEach((m1) => {
 
-        // Getting the current molecule
-        const molecule1 = molecules[i];
+        molecules.forEach((m2) => {
 
-
-        // Getting the bounding boxes of the faces and lines of the current molecule and their colors
-        const { facesBoxes: molecule1FacesBoxes, linesBoxes: molecule1LinesBoxes } = getBoundingBoxes(molecule1);
-
-        // Iterating through all the molecules again except the current molecule
-        for (var j = i + 1; j < molecules.length; j++) {
-
-            const molecule2 = molecules[j];
-
-            const { facesBoxes: molecule2FacesBoxes, linesBoxes: molecule2LinesBoxes } = getBoundingBoxes(molecule2);
-
-            // Checking if the bounding boxes of the faces and lines of the two molecules are intersecting
-            molecule1FacesBoxes.forEach((faceBox1: BoundingBox) => {
-
-                molecule2FacesBoxes.forEach((faceBox2: BoundingBox) => {
-                    if (faceBox1.box.intersectsBox(faceBox2.box) && faceBox1.color.equals(faceBox2.color)) {
-                        console.log(faceBox1.color);
-                    }
-                })
-
-                molecule2LinesBoxes.forEach((lineBox2: BoundingBox) => {
-                    if (faceBox1.box.intersectsBox(lineBox2.box) && faceBox1.color.equals(lineBox2.color)) {
-                        console.log(faceBox1.color);
-                    }
-                })
-            })
-
-            molecule1LinesBoxes.forEach((lineBox1: BoundingBox) => {
-
-                molecule2FacesBoxes.forEach((faceBox2: BoundingBox) => {
-                    if (lineBox1.box.intersectsBox(faceBox2.box) && lineBox1.color.equals(faceBox2.color)) {
-                        console.log(lineBox1.color);
-                    }
-                })
-
-                molecule2LinesBoxes.forEach((lineBox2: BoundingBox) => {
-                    if (lineBox1.box.intersectsBox(lineBox2.box) && lineBox1.color.equals(lineBox2.color)) {
-                        console.log(lineBox1.color);
-                    }
-                })
+            if (m1 !== m2) {
+                return checkBoundingBoxes(m1, m2);
             }
-            )
 
-        }
-    }
+        })
+    })
+
+
 };
